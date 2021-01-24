bf8804b338f8f92b80763c099f1f32c47c56748b
kanata@anythinq.net <kanata@anythinq.net>
2021-01-24 15:31:39 +0900

    プロジェクト ファイルを追加します。

---
+++ Practice ToDo/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Practice_ToDo
+{
+    /// <summary>
+    /// App.xaml の相互作用ロジック
+    /// </summary>
+
+    #region DBパスの指定
+    public partial class App : Application
+    {
+        static string databaseName = "ToDo.db";
+        static string folderPath = Environment.CurrentDirectory;
+        public static string DatabasePath = System.IO.Path.Combine(folderPath, databaseName);
+
+        public enum Priority
+        {
+            A = 1,
+            B = 2,
+            C = 3
+        }
+    }
+    # endregion
+}
